23bb0206674bb438206d5544fce69a8399c3f4bc
kimjongun83 <quocdattranhuu1606@gmail.com>
2020-11-16 18:58:40 +0700

    Changes 44

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb26932cae167001e069e3e",
+				"5fb26932cae167001e069e40"
+			]
+		},
+		"5fb26932cae167001e069e3e": {
+			"id": "5fb26932cae167001e069e3e",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb26932cae167001e069e40": {
+			"id": "5fb26932cae167001e069e40",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
